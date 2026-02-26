fd38bdd531c95ec70626c8dea9c056d6144676c2
Elite Lu <elitelulww@gmail.com>
2026-02-26 15:59:40 -0500

    regex fix

--- src/app/api/LastFM/route.ts
+++ src/app/api/LastFM/route.ts
@@ -320,16 +320,15 @@ const buildResult = async (
    for (const [artistName, lfmAlbum] of Object.entries(lfmAlbumMap)) {
       for (const [albumName, album] of Object.entries(lfmAlbum)) {
          // Remove the - Single or - EP suffixes for better matching
-         const removed = albumName.replace(
-            /\s+-\s*?(?:EP|Single|\(Deluxe(?: Edition)?\))$/i,
-            "",
-         );
+         const removed = albumName
+            .replace(/\s+-\s*?(?:EP|Single|\(Deluxe(?: Edition)?\))$/i, "")
+            .replace(/\s+?(?:EP|Single|\(Deluxe(?: Edition)?\))$/i, "");
 
          const cleanedName = albumName
             .replace(/\s+-\s*?(?:EP|Single|\(Deluxe(?: Edition)?\))$/i, "")
+            .replace(/\s+?(?:EP|Single|\(Deluxe(?: Edition)?\))$/i, "")
             .trim()
             .toLowerCase();
-
          addAlbumMapping(cleanedName, removed);
 
          // Check if the name of the artist exists in the lfmArtistsMap
